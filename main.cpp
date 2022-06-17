75bd06ae24ee89bed73e2abd0326e5eeb61f5cb3
IoT-Fish <antanas@stankevicius.dev>
2022-06-17 22:24:01 +0300

    feat: rod assembly feature ready

---
+++ main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <stdio.h>
+
+class RodPreparation {
+public: 
+    enum eBlankType { eTwoPeace, eTelescopic };
+    enum eReelType { eFeeder, eBaitcaster };
+    enum eRigType { eFreeRunning, eHelicopter };
+
+    RodPreparation(eBlankType blankType, eReelType reelType, eRigType rigType) :
+        blankType(blankType), reelType(reelType), rigType(rigType) {}
+
+    void assembleFishingRod(void) {
+        assembleBlank(blankType);
+        attachReel();
+        passLineThroughBlankRings(reelType);
+    }
+
+    void attachFishingRig();
+
+    void prepareFreeRunningRig();
+
+    void putOnWorm();
+
+private: 
+    eBlankType blankType;
+    eReelType reelType;
+    eRigType rigType;
+
+    void assembleBlank(eBlankType blankType) {
+        if (blankType == eTelescopic) {
+            printf("Assembling TELESCOPIC rod by SPREADING OUT PIECES\n");
+        } else if (blankType == eTwoPeace) {
+            printf("Assembling TWO PEACE rod by JOINING TWO PIECES\n");
+        } else {
+            printf("Unsupported BLANK type %u\n", blankType);
+        }
+    }
+
+    void attachReel(void) {
+        printf("Placing reel in reel seat and fasten it\n");
+    }
+
+    void passLineThroughBlankRings(eReelType reelType) {
+        releaseLine(reelType);
+        printf("Passing line through blank rings\n");
+    }
+
+    void releaseLine(eReelType reelType) {
+        if (reelType == eFeeder) {
+            printf("Releasing line with FEEDER reel by OPENING BAIL\n");
+        } else if (reelType == eBaitcaster) {
+            printf("Releasing line with BAITCASTER reel by PRESSING BUTTON\n");
+        } else {
+            printf("Unsupported REEL type %u\n", reelType);
+        }
+    }
+};
+
+int main (void) {
+    RodPreparation feederRod(RodPreparation::eTwoPeace, RodPreparation::eFeeder, RodPreparation::eFreeRunning);    
+    feederRod.assembleFishingRod();
+    
+    return 0;
+}
