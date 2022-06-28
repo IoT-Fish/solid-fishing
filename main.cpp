c0bfc169690c1557a88fc947f6e7b0bc3e81aa4e
IoT-Fish <antanas@stankevicius.dev>
2022-06-28 21:16:27 +0300

    feat: good example
    todo: split classes to different files

--- main.cpp
+++ main.cpp
@@ -1,24 +1,30 @@
 #include <iostream>
 #include <stdio.h>
 
-class FishingRod {
+class AssembleRod {
 public:
-    void assembleRod(void) {
+    void blank(void) {
         printf("Assembling TWO PEACE rod by JOINING TWO PIECES\n");
         printf("Placing reel in reel seat and fasten it\n");
         printf("Passing line through 10 blank RINGS\n");
     }
 
-    void joinRig(void) {
+    void rig(void) {
         printf("Tying FREE RUNNING rig\n");
     }
+};
 
-    void prepareHook(void) {
+class PrepareHook {
+public:
+    void worm(void) {
         printf("Pulling WORM down onto the hook\n");
     }
+};
 
+class RodAction {
+public:
     void cast(void) {
-        printf("Cast\n");
+        printf("Casting\n");
     }
 
     void retrieve(void) {
@@ -26,13 +32,36 @@ public:
     }
 };
 
+class FishingRodFacade: public AssembleRod, public PrepareHook, public RodAction {
+public:
+    void assembleRod(void) {
+        blank();
+    }
+
+    void joinRig(void) {
+        rig();
+    }
+
+    void prepareHook(void) {
+        worm();
+    }
+
+    void castRod(void) {
+        cast();
+    }
+
+    void retrieveRod(void) {
+       retrieve();
+    }
+};
+
 int main (void) {
-    FishingRod feederRod;
+    FishingRodFacade feederRod;
     feederRod.assembleRod();
     feederRod.joinRig();
     feederRod.prepareHook();
-    feederRod.cast();
-    feederRod.retrieve();
+    feederRod.castRod();
+    feederRod.retrieveRod();
 
     return 0;
 }
